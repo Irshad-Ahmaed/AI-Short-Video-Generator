b180e08ef2d2cb12ca17bde2e723297faa5c8cf0
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2024-10-27 21:10:13 +0530

    Frontend

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-black">Welcome to my website</h1>
+      <Button className="ml-1" size="sm">Subscribe</Button>
+    </div>
+  );
+}
